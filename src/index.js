d59c0fc66f19d956310df7c0b82640a435bc86fd
Greg <greg@Gregs-MacBook-Pro.local>
2017-07-26 08:55:20 -0400

    added some new dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux'
+import { h } from 'react-hyperscript'
+import counterApp from './store/reducers/index'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counterApp)
+
+class Application extends Component {
+    render() {
+        return (
+            h(Provider, {
+                store: store
+            }, [
+                    h(App)
+                ])
+        )
+    }
+}
+
+ReactDOM.render(
+    <Application></Application>
+    , document.getElementById('root'));
 registerServiceWorker();
